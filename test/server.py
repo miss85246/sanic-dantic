fa63407d9b8ffa48164dce06d591c994370586a4
Connor <63054779@qq.com>
2020-11-22 23:42:46 +0800

    --
    Signed-off-by: Connor <63054779@qq.com>

--- test/server.py
+++ test/server.py
@@ -1,5 +1,6 @@
 from sanic import Sanic
 from sanic.response import json
+from sanic.views import HTTPMethodView
 
 from sanic_dantic import parse_params, BaseModel
 
@@ -11,17 +12,45 @@ class Person(BaseModel):
     age: int
 
 
-class Car(BaseModel):
-    name: str
-    speed: int
+@app.route('/path_test/<name>/<age>/', methods=['GET'])
+@parse_params(path=Person)
+async def path_test(request, name, age, params):
+    return json({"params": params, "request.ctx.params": request.ctx.params})
 
 
-@app.route('/test', methods=['POST'])
-@parse_params(form=Car, body=Person)
-async def post_test(request, params):
-    print(params)
-    return json(params)
+@app.route('/query_test', methods=['GET'])
+@parse_params(query=Person)
+async def get_test(request, params):
+    return json({"params": params, "request.ctx.params": request.ctx.params})
 
+
+@app.route('/form_test', methods=['GET', 'POST'])
+@parse_params(form=Person)
+async def path_test(request, params):
+    return json({"params": params, "request.ctx.params": request.ctx.params})
+
+
+@app.route('/body_test', methods=['POST', 'PUT', 'PATCH'])
+@parse_params(body=Person)
+async def body_test(request, params):
+    return json({"params": params, "request.ctx.params": request.ctx.params})
+
+
+class SanicDanticTestView(HTTPMethodView):
+    @parse_params(query=Person)
+    async def get(self, request, params):
+        return json({"params": params, "request.ctx.params": request.ctx.params})
+
+    @parse_params(form=Person)
+    async def post(self, request, params):
+        return json({"params": params, "request.ctx.params": request.ctx.params})
+
+    @parse_params(body=Person)
+    async def put(self, request, params):
+        return json({"params": params, "request.ctx.params": request.ctx.params})
+
+
+app.add_route(SanicDanticTestView.as_view(), '/cbv_test')
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000)
--- test/test.py
+++ test/test.py
@@ -1,6 +1,48 @@
+from unittest import TestCase
+
 import requests
 
-form_data = {'name': 'bwm', 'speed': 100}
-body_data = {'name': 'connor', 'age': 13}
-res = requests.post('http://localhost:5000/test', json=body_data, data=form_data, )
-print(res.text)
+
+class TestSanicDantic(TestCase):
+    def setUp(self):
+        self.session = requests.session()
+        self.data = {"name": "test", "age": 100}
+        self.expected_result = {"params": self.data, "request.ctx.params": self.data}
+
+    def test_path_test(self):
+        url = f'http://localhost:5000/path_test/{self.data["name"]}/{self.data["age"]}/'
+        res = self.session.get(url).json()
+        self.assertEqual(res, self.expected_result), f"response: {res} \n expected_result: {self.expected_result}"
+
+    def test_get_test(self):
+        url = 'http://localhost:5000/query_test/'
+        res = self.session.get(url, params=self.data).json()
+        self.assertEqual(res, self.expected_result), f"response: {res} \n expected_result: {self.expected_result}"
+
+    def test_form_test(self):
+        url = 'http://localhost:5000/form_test/'
+        res = self.session.post(url, data=self.data).json()
+        self.assertEqual(res, self.expected_result), f"response: {res} \n expected_result: {self.expected_result}"
+
+    def test_body_test(self):
+        url = 'http://localhost:5000/body_test/'
+        res = self.session.post(url, json=self.data).json()
+        self.assertEqual(res, self.expected_result), f"response: {res} \n expected_result: {self.expected_result}"
+
+    def test_cbv_query_test(self):
+        url = 'http://localhost:5000/cbv_test/'
+        res = self.session.get(url, params=self.data).json()
+        self.assertEqual(res, self.expected_result), f"response: {res} \n expected_result: {self.expected_result}"
+
+    def test_cbv_form_test(self):
+        url = 'http://localhost:5000/cbv_test/'
+        res = self.session.post(url, data=self.data).json()
+        self.assertEqual(res, self.expected_result), f"response: {res} \n expected_result: {self.expected_result}"
+
+    def test_cbv_body_test(self):
+        url = 'http://localhost:5000/cbv_test/'
+        res = self.session.put(url, json=self.data).json()
+        self.assertEqual(res, self.expected_result), f"response: {res} \n expected_result: {self.expected_result}"
+
+    def tearDown(self):
+        self.session.close()
