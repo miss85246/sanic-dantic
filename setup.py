e3fa205cdb44104275ea939485e732f9646c9f5f
Connor Zhang <63054779@qq.com>
2022-05-08 14:59:40 +0800

    更新 setup.py 文件

--- setup.py
+++ setup.py
@@ -5,24 +5,24 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sanic-dantic",
-    version="1.1.9",
+    version="1.2.0",
     author="Connor Zhang",
     author_email="chzhangyue@outlook.com",
     description="A request parameter checking and parsing library based on pydantic under the sanic framework",
-    install_requires=['pydantic>=1.8.1'],
+    install_requires=['pydantic>=1.9.0'],
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/misss85246/sanic-dantic",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Utilities",
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: MIT License",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.8',
 )
