aea0a131ff855571e4b8eace2b509321b0542953
ConnorZhang <63054779@qq.com>
2023-01-24 23:45:31 +0800

    update setup.py, add 3.11 support

--- setup.py
+++ setup.py
@@ -1,29 +1,37 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+content = open("README.md", "r").read()
+lines = content.split("\n")
+
+packagename = "sanic-dantic"
+author = "Connor Zhang"
+author_email = "chzhangyue@outlook.com"
+version = "1.2.2"
+description = "".join(lines[11:13])
+long_description = "\n".join(lines)
 
 setuptools.setup(
-    name="sanic-dantic",
-    version="1.2.1",
-    author="Connor Zhang",
-    author_email="chzhangyue@outlook.com",
-    description="A request parameter checking and parsing library " +
-                "based on pydantic under the sanic framework",
-    install_requires=['pydantic>=1.9.1'],
+    author=author,
+    version=version,
+    name=packagename,
+    python_requires='>=3.7',
+    description=description,
+    author_email=author_email,
+    license_files=('LICENSE',),
     long_description=long_description,
+    packages=setuptools.find_packages(),
+    install_requires=['pydantic>=1.9.1'],
     long_description_content_type="text/markdown",
     url="https://github.com/misss85246/sanic-dantic",
-    packages=setuptools.find_packages(),
     classifiers=[
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "Topic :: Utilities",
+        "Programming Language :: Python :: 3.11",
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
-        "License :: OSI Approved :: MIT License",
-    ],
-    python_requires='>=3.8',
+    ]
 )
